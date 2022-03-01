b3d4ce349f79e66c33a6febdf7e652cc32f4b823
Russell-Ali <resul.eliyev2000@mail.ru>
2022-03-01 13:35:14 +0400

    password patch and some defaults for shorter run command

--- config.def.h
+++ config.def.h
@@ -3,11 +3,11 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 360;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Dejavu:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -19,9 +19,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 8;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 28;
 static unsigned int min_lineheight = 8;
 
 /*
@@ -31,4 +31,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 2;
